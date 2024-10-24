125eea11b825469d5e4f75a46e67095ca33a0951
annepham1512 <phamn@dickinson.edu>
2024-10-24 18:18:45 -0400

    Test report header's visibility after clicking Gen Report

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -45,4 +45,19 @@ describe("Test the harvest report default values", () => {
     // Check the number of crops, it is 112 because we also have "All crops"
     cy.get("[data-cy=crop-dropdown").children().should("have.length", 112);
   });
+
+  // it("Check generate button clickable", () => {
+  //   cy.get("[data-cy=generate-report-button]").click();
+  // });
+
+  it("Check if display the report header after generating the report", () => {
+    //Step 1: Before generating the report, ensure the report header does not exist
+    cy.get("[data-cy=report-header]").should("not.exist");
+
+    // Step 2: Click the Generate Report button
+    cy.get("[data-cy=generate-report-button").click();
+
+    // Step 3: After generating the report, ensure that the rpeort header is visible
+    cy.get("[data-cy=report-header").should("be.visible");
+  });
 });
